380f49e18e02f342fc65832aa50920ba72e55115
AT-Phillips <71952975+AT-Phillips@users.noreply.github.com>
2023-07-23 19:51:42 -0500

    Edits

---
+++ Boostrap lab/scripts.js
@@ -0,0 +1,2 @@
+//document.write("<footer class='text-light bg-dark'>Thank you!</footer>");
+document.getElementById ("theFooter").innterHTML = "<footer class='text-light bg-dark'>Thank you!</footer>";
