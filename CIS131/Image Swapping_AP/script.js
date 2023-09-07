4a0a5f9699a220ca869ef6f9caf09db006b17e6d
AT-Phillips <71952975+AT-Phillips@users.noreply.github.com>
2023-09-06 23:55:19 -0500

    final edits

--- CIS131/Image Swapping_AP/script.js
+++ CIS131/Image Swapping_AP/script.js
@@ -1,5 +1,8 @@
+/*Overall, this JavaScript passes a clicked image as an argument and updates the large image 
+based on the click action, in this case clicking one of the thumbnails below the large image*/
+
 function changeImage(thumbnail) {
     const largeImage = document.getElementById("large-image");
-    largeImage.src = thumbnail.src;
+    largeImage.src = thumbnail.src; /*This replaces the "src" of the large image with the "src" of the clicked image*/
     largeImage.alt = thumbnail.alt;
 }
