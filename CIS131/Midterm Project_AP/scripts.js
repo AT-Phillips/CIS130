a085d37838d0ecb4d851d0aebf5cc3c68101ce19
AT-Phillips <71952975+AT-Phillips@users.noreply.github.com>
2023-10-09 23:10:15 -0500

    Update scripts.js

--- CIS131/Midterm Project_AP/scripts.js
+++ CIS131/Midterm Project_AP/scripts.js
@@ -1,10 +1,13 @@
 //Austin Phillips, 23/FA-CIS-131-W01, 10/09/23, "Midterm", scripts.js
+
+//Get DOM elements
 var stopLight = document.getElementById('stop-light');
 var winnerImage = document.getElementById('winner');
 var player1 = document.getElementById('player1');
 var player2 = document.getElementById('player2');
 var raceContainer = document.getElementById('race-container');
 
+//Change 'stop' image to 'go' image
 function startRace() {
     stopLight.src = 'go.jpg';
 
@@ -16,18 +19,22 @@ function startRace() {
     var distance1 = 0;
     var distance2 = 0;
 
+    //Function to move player 1 in the race
     function movePlayer1() {
         distance1 += player1Advancement;
         player1.style.marginLeft = distance1 + 'px';
+        //Check if player 1 has reached the finish line
         if (distance1 >= raceContainer.clientWidth - player1.clientWidth) {
             clearInterval(interval1);
             declareWinner('Player 1', 'rabbitwon.jpg');  //Pass winner image for Player 1
         }
     }
 
+        //Function to move player 2 in the race
     function movePlayer2() {
         distance2 += player2Advancement;
         player2.style.marginLeft = distance2 + 'px';
+        //Check if player 1 has reached the finish line
         if (distance2 >= raceContainer.clientWidth - player2.clientWidth) {
             clearInterval(interval2);
             declareWinner('Player 2', 'turtlewon.jpg');  //Pass winner image for Player 2
@@ -50,6 +57,7 @@ function resetRace() {
     player1.style.marginLeft = '0';
     player2.style.marginLeft = '0';
 
+    //Switch back to 'stop' image
     stopLight.src = 'stop.jpg';
 
     winnerImage.style.display = 'none';
